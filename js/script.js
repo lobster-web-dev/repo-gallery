1506f9c1d9a041bbecd33222e504e4eff8d6c509
Joshua <galant.python.dev@gmail.com>
2024-03-13 08:46:37 -0500

    Fetched data from github to display repos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-//Where profile info will appear//
 const overview = document.querySelector(".overview");
 const username = "lobster-web-dev";
 const repoList = document.querySelector(".repo-list");
@@ -26,8 +25,20 @@ const displayUserInfo = function (data) {
     </div>
   `;
     overview.append(div);
+    gitRepos();
 };
 
 const gitRepos = async function () {
-    
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
